4eadcc73ed6ecf015a1c38b687b7a9e376420efd
rubences <91731294+rubences@users.noreply.github.com>
2024-02-27 14:55:47 +0000

    testeando

--- Test.py
+++ Test.py
@@ -1,6 +1,8 @@
 from copy import copy
 
 class Test:
+    def __init__(self):
+        self.algo = "Algo"
     pass
 
 test1 = Test()
@@ -9,6 +11,16 @@ test2 = copy(test1)
 test1.algo = "Prueba"
 
 print(test2 == test1)  # ¿Son el mismo objeto?
+
+lista1 = [1,2,3]
+lista2 = copy(lista1)
+lista1 = None
+
+print(lista1)
+print(lista2)
+None
+[1, 2, 3]
+
 
 try:
     print(test2.algo)
